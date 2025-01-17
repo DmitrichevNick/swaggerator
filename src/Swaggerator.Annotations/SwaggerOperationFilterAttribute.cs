09641d42420318eb26d09776ddda1d14be52d53f
foxocat <dmitrichev.nick@gmail.com>
2025-01-17 21:23:11 +0300

    added annotations

---
+++ src/Swaggerator.Annotations/SwaggerOperationFilterAttribute.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Swaggerator.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class SwaggerOperationFilterAttribute : Attribute
+    {
+        public SwaggerOperationFilterAttribute(Type filterType)
+        {
+            FilterType = filterType;
+        }
+
+        public Type FilterType { get; private set; }
+    }
+}
---
+++ src/Swaggerator.Annotations/SwaggerSubTypesAttribute.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Swaggerator.Annotations
+{
+    [Obsolete("Use multiple SwaggerSubType attributes instead")]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false)]
+    public class SwaggerSubTypesAttribute : Attribute
+    {
+        public SwaggerSubTypesAttribute(params Type[] subTypes)
+        {
+            SubTypes = subTypes;
+        }
+
+        public IEnumerable<Type> SubTypes { get; }
+
+        public string Discriminator { get; set; }
+    }
+}
