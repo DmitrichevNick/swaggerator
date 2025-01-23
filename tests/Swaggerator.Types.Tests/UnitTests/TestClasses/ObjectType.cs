17d2c9535c398582369fdd16783e325a1dd9cf59
foxocat <dmitrichev.nick@gmail.com>
2025-01-23 23:24:16 +0300

    added types tests

---
+++ tests/Swaggerator.Types.Tests/UnitTests/TestClasses/ObjectType.cs
@@ -0,0 +1,8 @@
+namespace Swaggerator.Types.Tests.UnitTests.TestClasses
+{
+    internal class ObjectType
+    {
+        public int Field1 { get; set; }
+        public short Field2 { get; set; }
+    }
+}
