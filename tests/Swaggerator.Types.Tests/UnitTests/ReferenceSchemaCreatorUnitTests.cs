064d7acb46edfd22ffb048bc8ad7e41525ed6057
foxocat <dmitrichev.nick@gmail.com>
2025-01-26 22:05:24 +0300

    added tests for rerference schema

---
+++ tests/Swaggerator.Types.Tests/UnitTests/ReferenceSchemaCreatorUnitTests.cs
@@ -0,0 +1,28 @@
+using Swaggerator.Types.SchemaCreators;
+using Swaggerator.Types.Schemas;
+using Swaggerator.Types.Tests.UnitTests.TestClasses;
+
+namespace Swaggerator.Types.Tests;
+
+[TestClass]
+public class ReferenceSchemaCreatorUnitTests
+{
+    [TestMethod]
+    public void Constructor_Parameterless_InstanceNotNull()
+    {
+        var creator = new ReferenceSchemaCreator();
+
+        Assert.IsNotNull(creator);
+    }
+
+    [TestMethod]
+    public void Create_AnyClass_HasSetReference()
+    {
+        const string _reference = "#/components/schemas/EmptyClass";
+        var creator = new ReferenceSchemaCreator();
+
+        var result = (ReferenceSchema)creator.Create(typeof(EmptyClass));
+
+        Assert.AreEqual(_reference, result.Ref);
+    }
+}
