e8bd519148c31b822e0a9c2e40d4798e11c6fa0b
foxocat <dmitrichev.nick@gmail.com>
2025-01-30 21:10:06 +0300

    added factory

---
+++ src/Swaggerator.Swagger/SchemasConverters/SchemaConverterFactory.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Swaggerator.Types.Schemas;
+
+namespace Swaggerator.Swagger.SchemasConverters
+{
+    public static class SchemaConverterFactory
+    {
+        public static ISchemaConverter Create(Schema schema)
+        {
+            var typeName = schema.GetType().Name;
+
+            switch (typeName)
+            {
+                case nameof(BooleanSchema):
+                    return new StringSchemaConverter();
+                case nameof(NumberSchema):
+                    return new StringSchemaConverter();
+                case nameof(IntegerSchema):
+                    return new StringSchemaConverter();
+                case nameof(StringSchema):
+                    return new StringSchemaConverter();
+                case nameof(ArraySchema):
+                    return new StringSchemaConverter();
+                case nameof(ObjectSchema):
+                    return new StringSchemaConverter();
+                default:
+                    throw new InvalidOperationException($"Cannot create {nameof(ISchemaConverter)} for type '{typeName}'");
+            }
+        }
+    }
+}
