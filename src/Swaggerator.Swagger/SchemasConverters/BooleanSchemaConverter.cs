5f6fccf4d3ff205a4ebf93b466f9f1a4fbe0ba54
foxocat <dmitrichev.nick@gmail.com>
2025-02-02 23:18:52 +0300

    added boolean schema converter

---
+++ src/Swaggerator.Swagger/SchemasConverters/BooleanSchemaConverter.cs
@@ -0,0 +1,35 @@
+using System;
+
+using Microsoft.OpenApi.Models;
+
+using Swaggerator.Types.Schemas;
+
+namespace Swaggerator.Swagger.SchemasConverters
+{
+    /// <summary>
+    ///     SchemaConverter for <see cref="BooleanSchema"/>
+    /// </summary>
+    public class BooleanSchemaConverter : ISchemaConverter
+    {
+        /// <inheritdoc />
+        public OpenApiSchema Convert(Schema schema)
+        {
+            if (schema == null) throw new ArgumentNullException(nameof(schema));
+
+            if (schema is BooleanSchema booleanSchema)
+                return Convert(booleanSchema);
+
+            throw new ArgumentException($"Cannot convert schema of type {schema.GetType()}");
+        }
+
+        private OpenApiSchema Convert(BooleanSchema booleanSchema)
+        {
+            var openApiSchema = new OpenApiSchema
+            {
+                Type = booleanSchema.Type
+            };
+
+            return openApiSchema;
+        }
+    }
+}
